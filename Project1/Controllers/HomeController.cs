99c5bdfebbe3f8886171fd44a2b83b98d8b21a3c
johnnyataisg <42900658+johnnyataisg@users.noreply.github.com>
2018-11-12 14:02:32 -0700

    2018

--- Project1/Controllers/HomeController.cs
+++ Project1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,9 +14,48 @@ namespace Project1.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Mission()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Mission(Mission mission)
+        {
+            if (mission.MissionName == "China Shanghai Mission")
+            {
+                mission.PresidentName = "Xi Jin Ping";
+                mission.Address = "1912 Democracy St, Shanghai";
+                mission.Language = "Mandarin Chinese";
+                mission.Climate = "Not too hot, mildly humid";
+                mission.Religion = "Atheism";
+            }
+            else if (mission.MissionName == "North Korea Pyongyang Mission")
+            {
+                mission.PresidentName = "Kim Jong Eun";
+                mission.Address = "123 George Washington Blvd, Pyongyang";
+                mission.Language = "Korean";
+                mission.Climate = "Politically sensitive";
+                mission.Religion = "Kim Ancestral Worship";
+            }
+            else if (mission.MissionName == "Spirit Prison Mission")
+            {
+                mission.PresidentName = "Jesus";
+                mission.Address = "Paradise Temple Work Mission Department Room AD-33";
+                mission.Language = "Multilingual";
+                mission.Climate = "Not applicable"; 
+                mission.Religion = "Awaiting Conversion-ism";
+            }
+            ViewBag.Output += "<div class=\"mission-textbox\">";
+            ViewBag.Output += "<h4>Mission Name: " + mission.MissionName + "</h4>";
+            ViewBag.Output += "<h4>President's Name: " + mission.PresidentName + "</h4>";
+            ViewBag.Output += "<h4>Mission Address: " + mission.Address + "</h4>";
+            ViewBag.Output += "<h4>Language Spoken: " + mission.Language + "</h4>";
+            ViewBag.Output += "<h4>Area Climate: " + mission.Climate + "</h4>";
+            ViewBag.Output += "<h4>Local Religion: " + mission.Religion + "</h4>";
+            ViewBag.Output += "</div>";
+            return View("MissionInformation");
         }
 
         public ActionResult About()
---
+++ Project1/Models/Mission.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project1.Models
+{
+    public class Mission
+    {
+        public string MissionName { get; set; }
+        public string PresidentName { get; set; }
+        public string Address { get; set; }
+        public string Language { get; set; }
+        public string Climate { get; set; }
+        public string Religion { get; set; }
+        public string Flag { get; set; }
+    }
+}
